96782e31ad8a0b8831528a3024e4130a497d6043
Ryan <tswizzle@live.co.uk>
2013-04-14 12:50:33 +0100

    A little further towards file creation

--- documap.pl
+++ documap.pl
@@ -21,18 +21,34 @@ description     VARCHAR(300),
 lat             FLOAT,
 lng             FLOAT,
 category        VARCHAR(120),
-country_code    VARCHAR(5),	
+country_code    VARCHAR(5),
 link            VARCHAR(120),
 
 PRIMARY KEY     (id)
 );
 ";
+# [^\\]\"(.*?)[^\\]\"
+# \\"\1\\"
+our $JS =
+"
+documentaries = [
+{latLng: [45.05, 135.00], name: \"Wild Russia - The Secret Forest\", description: \"Wild Russia is a landmark High Definition series charting a journey across this vast land that stretches from Europe to the Pacific Ocean.\", category: \"'new' 'nature'\", link: \"http://www.youtube.com/watch?v=z9n49ViHgvY\"},
+{latLng: [58.30, 93.99], name: \"Wild Russia - Siberia\", description: \"Wild Russia is a landmark High Definition series charting a journey across this vast land that stretches from Europe to the Pacific Ocean.\", category: \"'new' 'nature'\", link: \"http://www.youtube.com/watch?v=1a5s6Ouhaf8\"},
+{latLng: [52.13, 5.29], name: \"Carl Sagan's Cosmos: Dutch Golden Age (1/3)\", description: \"An excerpt from Carl Sagan's Cosmos seris\", category: \"'new' 'science'\", link: \"http://www.youtube.com/watch?v=GvY8dQQI13Q\"}
+];
+var open_files=[\"new\"]; var gdpData={};\$(document).ready(function(){\$(function(){var e,e=new jvm.WorldMap({container:\$(\"#map\"),map:\"world_mill_en\",regionsSelectable:true,regionsSelectableOne:true,markersSelectable:true,markersSelectableOne:true,series:{regions:[{values:gdpData,scale:[\"#C8EEFF\",\"#0071A4\"],normalizeFunction:\"polynomial\"}]},onRegionLabelShow:function(e,t,n){if(!gdpData[n]){var r=0}else{var r=gdpData[n]}t.html(t.html()+\" (Documentaries: \"+r+\")\")},normalizeFunction:\"polynomial\",hoverOpacity:.4,hoverColor:false,markerStyle:{initial:{fill:\"#0071A4\",stroke:\"#fff\"},hover:{stroke:\"#fff\",r:7},selected:{fill:\"#000\"}},regionStyle:{selected:{fill:\"#7070B6\"}},onRegionSelected:function(e,t,n){\$(\"#other_info\").html(\"<p>\"+t+\"</p>\")},onMarkerSelected:function(e,t,n){name=arr[t][\"name\"];for(var r=0;r<documentaries.length;r++){a_name=documentaries[r][\"name\"];if(a_name==name){\$(\"#doc_info\").html(\"<h2>\"+documentaries[r][\"name\"]+\"</h2>\"+'<p><label>Link: </label><a href=\"'+documentaries[r][\"link\"]+'\">Click</a></p><p>'+documentaries[r][\"description\"]+\"<p>\")}}},backgroundColor:\"#383f47\"});\$(\".checkbox\").click(function(){window.arr=[];w_cats=\$(\"#cat_f\").serializeArray();e.removeAllMarkers();for(var t=0;t<w_cats.length;t++){wanted_cat=w_cats[t][\"name\"];fl_js=wanted_cat.replace(/\'/g,\"\");if(\$.inArray(fl_js,open_files)==-1){open_files.push(fl_js);\$.getScript(\"js/\"+fl_js+\".js\",function(t,n,r){for(var i=0;i<documentaries.length;i++){a_cat=documentaries[i][\"category\"];if(a_cat.indexOf(wanted_cat)!==-1){var s={latLng:documentaries[i][\"latLng\"],name:documentaries[i][\"name\"]};window.arr.push(s)}}e.addMarkers(window.arr)})}else{for(var n=0;n<documentaries.length;n++){a_cat=documentaries[n][\"category\"];if(a_cat.indexOf(wanted_cat)!==-1){var r={latLng:documentaries[n][\"latLng\"],name:documentaries[n][\"name\"]};window.arr.push(r)}}e.addMarkers(window.arr)}}});\$('input[type=\"checkbox\"]').prop(\"checked\",false);\$(\"input[name=\\\"'new'\\\"]\").click();\$(\".test\").click(function(){\$.getScript(\"js/added.js\")})})})
 
+";
+
+open(TFILE, ">>test2.txt");
+print TFILE $JS;
+close(TFILE);
+die();
 # Connect to DB
 our $dbh = DBI->connect("DBI:mysql:$database:localhost:$port", $user, $pass)
 	or die "Error connecting to MySQL Database:" . $DBI::errstr;
 
-# Test a query 
+# Test a query
 my $query_h = $dbh->prepare($tbl_struct);
 
 $query_h->execute() # Creates the table - if it doesn't exist
@@ -56,11 +72,11 @@ sub create_static_doc {
 		my $query = "SELECT DISTINCT category FROM $table";
 		my $query_h = $dbh->prepare($query);
 		$query_h->execute();
-	
+
 		if ($query_h->rows == 0) {
 			return "No documentaries found.\n";
 		}
-	
+
 		while (my @data = $query_h->fetchrow_array()) {
 			#print "bleh";#my $data;
 			print $data[0];
@@ -68,9 +84,9 @@ sub create_static_doc {
 		}
 		# This will create js/new.js
 		# create_js_cat();
-		
+
 	} # At this point, all js/*.js files should be created
-	# 
+	#
 	#
 	#	Once the categories have be done
 	#	Create the index.html
@@ -84,6 +100,7 @@ sub create_js_cat {
 	if (length($_[0] // '')) {
 		$var_nam = $_[0];
 	}
+
 	my $category = '%category%';
 	my $query = "SELECT id, name, lat, lng, description, category, link, country_code FROM $table WHERE category LIKE ?";
 	my $query_h = $dbh->prepare($query);
@@ -95,6 +112,7 @@ sub create_js_cat {
 	my $xx;
 	while (my @data = $query_h->fetchrow_array()) {
 		$entry = sprintf("{latLng: [%.2f, %.2f], name: \"%s\", description: \"%s\", category: \"%s\", link: \"%s\"},\n", $data[2], $data[3], $data[1], $data[4], $data[5], $data[6]);
+		# [2] [3] - LatLng, [1] - Name, [4] - Description, [5] - Category, [6] - Link
 		$file_c .= $entry;
 		# Count Country_codes
 		my $cc = $data[7];
@@ -105,29 +123,30 @@ sub create_js_cat {
 	}
 	$gdp = substr($gdp, 0, -2) . "\n};\n\n";
 	$file_c = substr($file_c, 0, -2) . "\n];\n\n";
-	
+
 	open(JSFILE, ">>js/$var_nam.js");
 	print JSFILE $file_c;
-	
+
 	if ($var_nam ne "documentaries" ) {
 		print JSFILE "documentaries = documentaries.concat($var_nam);\n";
 		close(JSFILE);
 	} else {
 		print JSFILE $gdp;
+		print JSFILE $JS; # The large frontend Javascript
 		#my $c_codes = "var gdpData = {};";
 	}
 	$query_h->finish;
 	return 0;
 }
 
-# Check for dead links 
+# Check for dead links
 sub check_dead_links {
 	my $rem_links = 0;
 	if (length($_[0] // '') && $_[0] == 1 ) {
 		$rem_links = 1;
 	}
 	my $query = "SELECT link FROM $table";
-		
+
 	print $rem_links;
 }
 
@@ -140,153 +159,10 @@ sub insert_doc {
 }
 
 #insert_doc();
-#create_js_cat();
+create_js_cat();
 #create_static_doc();
 #check_dead_links(1);
 
 
 $dbh->disconnect;
 
-our $JS =
-"
-//
-// 09/04/13 - 18:33
-//
-//
-var open_files = ['new'];
-
-	\$(document).ready(function() {
-		\$(function(){
-			var map,
-
-			map = new jvm.WorldMap({
-				container: \$('#map'),
-				map: 'world_mill_en',
-				regionsSelectable: true,
-				regionsSelectableOne: true,
-				markersSelectable: true,
-				markersSelectableOne: true,
-				series: {
-					regions: [{
-						values: gdpData,
-						scale: ['#C8EEFF', '#0071A4'],
-						normalizeFunction: 'polynomial'
-					}],
-				},
-				onRegionLabelShow: function(event, label, index) {
-					if (! gdpData[index]) {
-						var total_docs = 0;
-					} else {
-						var total_docs = gdpData[index];
-					}
-					label.html(label.html()+' (Documentaries: '+total_docs+')');
-				},
-				//scaleColors: ['#C8EEFF', '#0071A4'],
-				normalizeFunction: 'polynomial',
-				hoverOpacity: 0.4,
-				hoverColor: false,
-				markerStyle: {
-					initial: {
-						fill: '#0071A4',
-						stroke: '#fff'
-					},
-					hover: {
-						stroke: '#fff',
-						r: 7
-					},
-					selected: {
-						fill: '#000'
-					}
-				},
-				regionStyle: {
-					selected: {
-						fill: '#7070B6'
-					}
-				},
-				onRegionSelected: function(e, el, code) {
-					\$('#other_info').html(\"<p>\"+el+\"</p>\");
-				},
-				onMarkerSelected: function(e, el, code) {
-					name = arr[el][\"name\"];
-					// Needs to be cleaned up -
-					for (var p = 0; p < documentaries.length; p++) {
-						a_name = documentaries[p][\"name\"];
-						if (a_name == name) {
-							\$('#doc_info').html(
-								\"<h2>\"+documentaries[p][\"name\"] + \"</h2>\"+
-								//\"<p>\"+documentaries[p][\"category\"]+
-								\"<p><label>Link: </label><a href=\\\"\"+documentaries[p][\"link\"]+\"\\\">Click</a></p><p>\" +
-								documentaries[p][\"description\"]+\"<p>\"
-							);
-						}
-					}
-				},
-				backgroundColor: '#383f47'
-			});
-			\$(\".checkbox\").click(function() {
-				//\$.inArray(value, array)
-				window.arr=[];
-
-				w_cats = \$(\"#cat_f\").serializeArray();
-
-				map.removeAllMarkers();
-
-				for (var i = 0; i < w_cats.length; i++) {
-					wanted_cat = w_cats[i][\"name\"];
-
-					fl_js = wanted_cat.replace(/\\'/g, '');
-					// If the file isn't in the open array, add to array and open file
-					if (\$.inArray(fl_js, open_files) == -1) {
-						open_files.push(fl_js);
-
-						\$.getScript(\"js/\"+ fl_js + '.js', function(data, textStatus, jqxhr) {
-						for (var p = 0; p < documentaries.length; p++) {
-							a_cat = documentaries[p][\"category\"];
-							//If the wanted category is a substring of a_cat
-							if (a_cat.indexOf(wanted_cat) !== -1) {
-								var obj = {
-									latLng: documentaries[p][\"latLng\"],
-									name: documentaries[p][\"name\"]
-								};
-								window.arr.push(obj);
-							}
-						}
-						map.addMarkers(window.arr);
-						});
-
-					} else {
-					for (var p = 0; p < documentaries.length; p++) {
-						a_cat = documentaries[p][\"category\"];
-						//If the wanted category is a substring of a_cat
-						if (a_cat.indexOf(wanted_cat) !== -1) {
-							var obj = {
-								latLng: documentaries[p][\"latLng\"],
-								name: documentaries[p][\"name\"]
-							};
-							window.arr.push(obj);
-						}
-					}
-					map.addMarkers(window.arr);
-					}
-				}
-		});
-		// Checks the new category, also running the function to add markers
-		\$('input[type=\"checkbox\"]').prop(\"checked\", false);
-		\$('input[name=\"\\'new\\'\"]').click();
-
-		\$(\".test\").click(function() {
-			//
-			\$.getScript(\"js/added.js\");
-		});
-
-	});
-});
-
-
-//var arr= [];
-
-";
-
-#open(TFILE, ">>test.txt");
-#print TFILE $JS;
-#close(TFILE);
